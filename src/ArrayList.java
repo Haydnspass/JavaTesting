6bd28dceb5324d571b7250e414840b2410e526e8
Lucas-Raphael Müller <lucasraphaelmueller@gmail.com>
2017-01-17 15:40:21 +0100

    Update

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -4,8 +4,8 @@
 public class ArrayList {
     public static void main(String[] args)
     {
-        ArrayList<String> friends =  new ArrayList<String>();
-        friends.add("Peter");
-        friends.add("Paul");
+        // ArrayList<String> friends =  new ArrayList<String>();
+        // friends.add("Peter");
+        // sfriends.add("Paul");
     }
 }
